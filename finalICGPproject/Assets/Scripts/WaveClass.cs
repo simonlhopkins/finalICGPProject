424b25e9b6a233e9f209656070a7ed3d060528c9
simonlhopkins <simonlhopkins@gmail.com>
2018-11-29 16:23:44 +0100

    working on making wave/level classes

---
+++ finalICGPproject/Assets/Scripts/WaveClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveClass : MonoBehaviour {
+
+
+    private List<EnemyBaseClass> enemiesInWave;
+    public List<EnemyBaseClass> EnemiesInWave{
+        get
+        {
+            return enemiesInWave;
+        }
+        set
+        {
+            enemiesInWave = value;
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
